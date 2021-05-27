3d3c5ab6cbfa2c6175e4f4bdd76439fe4ad0ba37
Kuba Zastocki <kubazastocki@gmail.com>
2021-05-27 19:16:06 +0200

    Dodaje program 1

---
+++ ProjektPO/ProjektPO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjektPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!srutututu");
+        }
+    }
+}
+
